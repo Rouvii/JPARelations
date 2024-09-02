ffa2d4192fcab5b4c2e1e92e9df65dd8f975a227
Rouvii <115996187+Rouvii@users.noreply.github.com>
2024-09-02 17:19:29 +0200

    1:m relations

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,11 @@ package org.example;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import org.example.persistence.entities.Fee;
 import org.example.persistence.entities.Person;
 import org.example.persistence.entities.PersonDetail;
+
+import java.time.LocalDate;
 
 public class Main {
 
@@ -18,6 +21,11 @@ public class Main {
           Person p1 = new Person("Rouvi");
           PersonDetail pd1 = new PersonDetail("Middelgrundsvej", "KBH", "2300", 25);
           p1.setPersonDetail(pd1);
+          Fee f1 = new Fee(100, LocalDate.of(2024,9,2));
+          Fee f2 = new Fee(150, LocalDate.of(2024,10,2));
+
+          p1.addFee(f1);
+            p1.addFee(f2);
 
           em.getTransaction().begin();
           em.persist(p1);
